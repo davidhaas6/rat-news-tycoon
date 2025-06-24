f04552a86fb2f9cfaf5a89f2ab6701f3c577fc00
David Haas <davidhaas6@gmail.com>
2025-06-24 16:09:44 -0400

    mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,16 @@ import { useGameLoop } from './hooks/useGameLoop';
 export default function App() {
   useGameLoop();
   return (
-    <main className="grid grid-rows-[auto_1fr_auto] grid-cols-5 gap-2 w-screen h-dvh overflow-hidden">
-      {/* <div className="w-full table"></div> */}
+    <main className="grid grid-cols-1 md:grid-cols-5 grid-rows-[auto_1fr_auto] gap-2 w-screen h-dvh overflow-hidden">
+      <div className="col-span-full"><PlaceholderPanel text='top panel' /></div>
 
-      <div className="col-span-5"><PlaceholderPanel /></div>
+      {/* middle row → re-orders on small screens */}
+      <div className="order-2 md:order-none"><FinancePanel /></div>
+      <div className="order-1 md:order-none md:col-span-3"><Screen /></div>
+      <div className="order-3 md:order-none"><PlaceholderPanel text='right panel' /></div>
 
-      <FinancePanel />
-      <div className="col-span-3"><Screen /></div>
-      <PlaceholderPanel />
-
-      <div className="col-span-5"><PlaceholderPanel /></div>
-
+      <div className="order-4 col-span-full"><PlaceholderPanel text='Bottom' /></div>
     </main>
+
   );
 }
--- src/components/dash/PlaceholderPanel.tsx
+++ src/components/dash/PlaceholderPanel.tsx
@@ -1,11 +1,11 @@
 import { useGame } from '../../stores/useGame';
 
-export default function PlaceholderPanel() {
+export default function PlaceholderPanel(props: {text?: string}) {
   const cash = useGame(s => s.cash);
   return (
     <section className="rounded bg-stone-800 text-stone-100 p-4 border">
       <h2 className="text-lg font-bold">Header</h2>
-      <p>Some details here</p>
+      <p>{props.text ?? "Placeholder text"}</p>
       <button>
         Button
       </button>
